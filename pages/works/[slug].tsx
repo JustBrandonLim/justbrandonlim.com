dff02429b94e07aac75a60880f0b57e4d90b955f
Brandon <brandonlimjingzhi@gmail.com>
2022-05-16 13:46:13 +0800

    Added missing SearchEngineOptimization to some pages.

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -1,5 +1,6 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import SiteConfig from "../../site.config";
+import SearchEngineOptimization from "@components/SearchEngineOptimization";
 
 interface Work {
   title: string;
@@ -18,38 +19,41 @@ interface WorkProps {
 const Work: NextPage<WorkProps> = (workProps) => {
   const work: Work = workProps.work;
   return (
-    <div className="min-h-screen">
-      <section className="container max-w-2xl gap-5 p-5">
-        <h1 className="mb-5 text-3xl font-bold text-white md:text-5xl">{work.title}</h1>
-        <div className="flex flex-col gap-5 p-5 rounded-lg shadow-lg bg-zinc-700">
-          <p className="text-slate-300">{work.description}</p>
-          <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
-            <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">STACK</p>
-            <p className="text-slate-300">{work.stack}</p>
+    <>
+      <SearchEngineOptimization title={work.title} url={"works/" + work.slug} />
+      <div className="min-h-screen">
+        <section className="container max-w-2xl gap-5 p-5">
+          <h1 className="mb-5 text-3xl font-bold text-white md:text-5xl">{work.title}</h1>
+          <div className="flex flex-col gap-5 p-5 rounded-lg shadow-lg bg-zinc-700">
+            <p className="text-slate-300">{work.description}</p>
+            <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
+              <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">STACK</p>
+              <p className="text-slate-300">{work.stack}</p>
+            </div>
+            {work.platform ? (
+              <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
+                <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">PLATFORM</p>
+                <p className="text-slate-300">{work.platform}</p>
+              </div>
+            ) : null}
+            {work.github ? (
+              <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
+                <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">GITHUB</p>
+                <a
+                  className="underline text-sky-500 underline-offset-2 hover:text-sky-400"
+                  href={work.github}
+                  aria-label="GitHub"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {work.title}
+                </a>
+              </div>
+            ) : null}
           </div>
-          {work.platform ? (
-            <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
-              <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">PLATFORM</p>
-              <p className="text-slate-300">{work.platform}</p>
-            </div>
-          ) : null}
-          {work.github ? (
-            <div className="flex flex-col items-start gap-5 md:items-center md:flex-row">
-              <p className="p-1 text-sm font-bold text-black bg-teal-300 rounded-lg shadow-lg">GITHUB</p>
-              <a
-                className="underline text-sky-500 underline-offset-2 hover:text-sky-400"
-                href={work.github}
-                aria-label="GitHub"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {work.title}
-              </a>
-            </div>
-          ) : null}
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 };
 
