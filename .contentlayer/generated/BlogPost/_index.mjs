3805e77a27d671610677338ec4408e66d2224319
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2023-03-15 14:07:16 +0800

    chore: add new blog post.

--- .contentlayer/generated/BlogPost/_index.mjs
+++ .contentlayer/generated/BlogPost/_index.mjs
@@ -2,5 +2,6 @@
 
 import _5UsefulResourcesForWebDevelopersMdx from './_5-useful-resources-for-web-developers.mdx.json' assert { type: 'json' }
 import helloWorldMdx from './hello-world.mdx.json' assert { type: 'json' }
+import killingNodejsOnMacosMdx from './killing-nodejs-on-macos.mdx.json' assert { type: 'json' }
 
-export const allBlogPosts = [_5UsefulResourcesForWebDevelopersMdx, helloWorldMdx]
+export const allBlogPosts = [_5UsefulResourcesForWebDevelopersMdx, helloWorldMdx, killingNodejsOnMacosMdx]
