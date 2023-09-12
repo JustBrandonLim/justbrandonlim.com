e766804a182543a59eaac057e9cf42eeb4d7a6f1
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2023-09-12 09:31:52 +0800

    Added vercel analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import Header from "@components/shared/header";
 import Footer from "@components/shared/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"], variable: "--font-jetbrains-mono" });
@@ -67,6 +68,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <footer>
           <Footer />
         </footer>
+        <Analytics />
       </body>
     </html>
   );
